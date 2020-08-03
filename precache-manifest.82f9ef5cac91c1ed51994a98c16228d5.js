d8f8dbb2b7e0972d10036cddc2010ddcc2718ae3
abhijeet <slashkirkmetallica@gmail.com>
2020-08-04 02:08:16 +0530

    Updates

---
+++ precache-manifest.82f9ef5cac91c1ed51994a98c16228d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dd5bb3cac2dbb5f674daa6e5684d81",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "459250be97f2fe593488",
+    "url": "/monster-rolodex/static/css/main.3b5289e2.chunk.css"
+  },
+  {
+    "revision": "23bd912cc46755abb08b",
+    "url": "/monster-rolodex/static/js/2.55d989c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.55d989c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459250be97f2fe593488",
+    "url": "/monster-rolodex/static/js/main.1d0a24cc.chunk.js"
+  },
+  {
+    "revision": "73b02b2f0863eae34822",
+    "url": "/monster-rolodex/static/js/runtime-main.b6d76cec.js"
+  }
+]);
